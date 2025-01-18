82f3dfb7f4fa997e9dc5ef971097d0d62f4c0154
Akash K P <kpakash03@gmail.com>
2025-01-18 22:20:41 +0530

    first commit of spring boot

---
+++ src/test/java/com/infosys/sample_springBoot/SampleSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.sample_springBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
